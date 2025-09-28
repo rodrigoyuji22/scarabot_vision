5c47e6578c1aaa857ac2080791c0007922e6a296
Rodrigo <rodrigoyujik@gmail.com>
2025-09-27 23:25:42 -0300

    ativar cudas da placa de video + resolucao da camera

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from ultralytics import YOLO
 import cv2
+# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 (usar a gpu para rodar o modelo)
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 model = YOLO("best.pt")
-
 model.predict(
-    source = 1,
-    imgsz = 640,
+    source = 0,
+    imgsz = 800,
     conf=0.45,
     iou=0.5,
     vid_stride=2,
     show=True,
     save=False
 )
-
