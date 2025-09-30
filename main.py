29fd8c1b4877723400e4da7316173590574912c7
Rodrigo <rodrigoyujik@gmail.com>
2025-09-30 17:07:24 -0300

    main

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from vision import Vision
 from pathlib import Path
 from config import YOLO_PATH, CAM_ID, FRAME_WIDTH, FRAME_HEIGHT
 
-cap = Vision(CAM_ID, YOLO_PATH)
-cap.config_prop(FRAME_WIDTH, FRAME_WIDTH)
-cap.run_vision()
+def main():
+    cap = Vision(CAM_ID, YOLO_PATH)
+    cap.config_prop(FRAME_WIDTH, FRAME_WIDTH)
+    cap.run_vision()
 
